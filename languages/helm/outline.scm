9c14c9b1b650183bc75eca4a4e0af5ed885e4eab
Tom Zaspel <tom@zaspel.it>
2026-04-30 19:26:03 +0200

    feat: add new treesitter functions

---
+++ languages/helm/outline.scm
@@ -0,0 +1,4 @@
+; Capture named template definitions so they appear in Zed's outline panel.
+; e.g. {{- define "mychart.labels" -}} ... {{- end }}
+(define_action
+  (interpreted_string_literal) @name) @item
